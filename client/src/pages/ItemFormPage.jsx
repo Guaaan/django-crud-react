99d3f633474c07d81267e5808df67dd5ff6ee986
guanro <juampablo.txt@gmail.com>
2025-05-22 21:06:57 -0400

    avances en el form que agrega items

--- client/src/pages/ItemFormPage.jsx
+++ client/src/pages/ItemFormPage.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
 
 const ItemsFormPage = () => {
+      const {register, handleSubmit, formState:{
+            errors
+      }} = useForm();
+      const onSubmit = handleSubmit((data) => {
+            console.log(data);
+      });
       return (
             <div>
-                  <h1>Create Items Page</h1>
+                  <form onSubmit={onSubmit}>
+                        <input type="text" placeholder="title" 
+                        {...register("title" ,{required: true})}
+                        />
+                        {errors.title && <span>This field is required</span>}
+                        <textarea rows="3" placeholder="Description"
+                        {...register("description" ,{required: true})}></textarea>
+                        {errors.description && <span>This field is required</span>}
+                        <button>save</button>
+                  </form>
             </div>
       );
-};
+}; 
 
 export default ItemsFormPage;
